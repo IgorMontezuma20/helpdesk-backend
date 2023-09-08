963a6c034e5a84cd79cb6341338f28090286bab7
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-08 16:31:13 -0300

    feat: implementando o CRUD para entidade Cliente

--- src/main/java/com/IgorMontezuma20/helpdesk/repositories/ClienteRepository.java
+++ src/main/java/com/IgorMontezuma20/helpdesk/repositories/ClienteRepository.java
@@ -2,8 +2,8 @@ package com.IgorMontezuma20.helpdesk.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.IgorMontezuma20.helpdesk.domain.Pessoa;
+import com.IgorMontezuma20.helpdesk.domain.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Pessoa, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
