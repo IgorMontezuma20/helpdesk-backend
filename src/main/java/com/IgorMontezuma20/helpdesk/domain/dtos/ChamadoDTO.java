9124428f50d5c8b37c174959b536ffac7a8f97a8
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-09 11:09:36 -0300

    feat: implementando o metodo de create na entideda Chamado

--- src/main/java/com/IgorMontezuma20/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/IgorMontezuma20/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.IgorMontezuma20.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.IgorMontezuma20.helpdesk.domain.Chamado;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -17,12 +19,24 @@ public class ChamadoDTO implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 
+	@NotNull(message = "O campo PRIORIDADE é obrigatório.")
 	private Integer prioridade;
+	
+	@NotNull(message = "O campo STATUS é obrigatório.")
 	private Integer status;
+	
+	@NotNull(message = "O campo TÍTULO é obrigatório.")
 	private String titulo;
+	
+	@NotNull(message = "O campo OBSERVAÇÕES é obrigatório.")
 	private String observacoes;
+	
+	@NotNull(message = "O campo TÉCNICO é obrigatório.")
 	private Integer tecnico;
+	
+	@NotNull(message = "O campo CLIENTE é obrigatório.")
 	private Integer cliente;
+	
 	private String nomeTecnico;
 	private String nomeCliente;
 
