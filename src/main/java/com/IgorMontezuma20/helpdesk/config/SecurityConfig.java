1696fd10496aacb62a32fe93ac3dd3c5d1c0a5d4
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-11 11:35:06 -0300

    feat: configurando a autorizacao

--- src/main/java/com/IgorMontezuma20/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/IgorMontezuma20/helpdesk/config/SecurityConfig.java
@@ -5,11 +5,13 @@ package com.IgorMontezuma20.helpdesk.config;
 import java.util.Arrays;
 
 import com.IgorMontezuma20.helpdesk.security.JWTAuthenticationFilter;
+import com.IgorMontezuma20.helpdesk.security.JWTAuthorizationFilter;
 import com.IgorMontezuma20.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +26,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
@@ -47,6 +50,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter( authenticationManager(),jwtUtil));
+
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
